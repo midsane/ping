0066befa12f7a772043a49ec5c302f05ae24b4e4
“AdityaRaj150” <adityaraj10544@gmail.com>
2025-01-01 11:41:18 +0530

    updated urls

--- api/server.js
+++ api/server.js
@@ -1,15 +1,17 @@
-import express from "express"
+import express from "express";
 import fetch from "node-fetch";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get('/', (req,res) => {
-  res.status(200).send("healthy")
-})
+app.get("/", (req, res) => {
+  res.status(200).send("healthy");
+});
 
 const pingWebsite = (url) => {
   const now = new Date();
@@ -17,24 +19,20 @@ const pingWebsite = (url) => {
   const minutes = String(now.getMinutes()).padStart(2, "0");
   const seconds = String(now.getSeconds()).padStart(2, "0");
   const exactTime = `${hours}:${minutes}:${seconds}`;
-  fetch(url)
+  fetch(url);
   console.log(`pinged ${url} at ${exactTime}`);
 };
 
-
-const pingInterval = 3000;
+const pingInterval = 5000;
 const websiteUrl1 = "https://anime-blog-wwzt.onrender.com/ping";
-const websiteUrl2 = "https://taskify-backend-j23x.onrender.com";
+const websiteUrl2 = "https://taskify-backend-j23x.onrender.com/ping";
+const websiteUrl3 = "https://midblade.onrender.com/ping";
 
-// Start the interval to ping every minute
 setInterval(() => {
   pingWebsite(websiteUrl1);
-}, pingInterval);
-
-setInterval(() => {
   pingWebsite(websiteUrl2);
+  pingWebsite(websiteUrl3);
 }, pingInterval);
 
-
-export default app
+export default app;
 
